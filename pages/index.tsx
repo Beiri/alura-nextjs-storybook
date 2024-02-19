3dce57716e3f254dc82e3a0a7566a975d2a52ebd
Beiri <beiri.al@outlook.com>
2024-02-19 20:32:10 -0300

    Added styled-components, theme settings, theme provider and theme documentation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
 import Button from "../src/components/Button/Button";
+import ThemeProvider from "../src/theme/ThemeProvider";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Homepage</h1>
-      <Button>Simple button</Button>
-    </div>
+    <ThemeProvider>
+      <div>
+        <h1>Homepage</h1>
+        <Button variant="accent">Simple button</Button>
+      </div>
+    </ThemeProvider>
+
   )
 }
