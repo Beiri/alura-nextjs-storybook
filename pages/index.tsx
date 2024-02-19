fdfb5e025098e71c9d962ab581549e235f9d4724
Beiri <beiri.al@outlook.com>
2024-02-19 18:44:23 -0300

    Example story

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
+import Button from "../src/components/Button/Button";
+
 export default function Home() {
   return (
     <div>
       <h1>Homepage</h1>
+      <Button>Simple button</Button>
     </div>
   )
 }
